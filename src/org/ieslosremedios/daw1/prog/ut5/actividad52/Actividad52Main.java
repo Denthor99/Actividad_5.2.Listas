9bdf338ccde6a12ffdfaa43b5cebdf572fb6eae8
Den <drodsan2708@g.educaand.es>
2023-03-02 13:56:34 +0100

    drodsan2708 - Subida del ejercicio a github - 02/03/2023

--- src/org/ieslosremedios/daw1/prog/ut5/actividad52/Actividad52Main.java
+++ src/org/ieslosremedios/daw1/prog/ut5/actividad52/Actividad52Main.java
@@ -5,31 +5,29 @@ public class Actividad52Main{
 
     public static void main(String[] args) {
         System.out.println("           Lista de Tareas   ");
-        TaskList tareas=new TaskList("ArrayList");
-        System.out.println("Opciones disponibles");
-        System.out.println("__________________________________");
+        TaskList tareas=new TaskList("LinkedList");
+        System.out.println("         Opciones disponibles");
+        System.out.println("________________________________________");
         System.out.println("1. Agrega una nueva tarea a la lista");
+        tareas.addTask("Tomar apuntes");
+        tareas.addTask("Apuntar posibles dudas");
+        tareas.addTask("Realizar los ejercicios correspondientes");
+        tareas.addTask("Preguntar las dudas al profesor");
+        tareas.addTask("Estudiar para el examen");
         System.out.println();
-        tareas.addTask(listaTareas"Tomar apuntes");
-        TaskList.addTask(lista,"Apuntar posibles dudas");
-        TaskList.addTask(lista,"Realizar los ejercicios correspondientes");
-        TaskList.addTask(lista,"Preguntar las dudas al profesor");
-        TaskList.addTask(lista,"Estudiar para el examen");
         System.out.println("2. Eliminar una tarea de la lista");
-        System.out.println();
-        TaskList.removeTask(lista,"Preguntar las dudas al profesor");
-        TaskList.removeTask(lista,"Apuntar posibles dudas");
+        tareas.removeTask("Preguntar las dudas al profesor");
+        tareas.removeTask("Apuntar posibles dudas");
         System.out.println();
         System.out.println("3. Completar alguna de las tareas");
-        System.out.println();
-        TaskList.completeTask(lista,"Tomar apuntes");
+        tareas.completeTask("Tomar apuntes");
         System.out.println();
         System.out.println("4. Imprimir todas las tareas");
-        System.out.println();
         for (String e:
-                lista) {
+                tareas.getTasks()) {
             System.out.println(e);
         }
+
 
 
     }
--- src/org/ieslosremedios/daw1/prog/ut5/actividad52/TaskList.java
+++ src/org/ieslosremedios/daw1/prog/ut5/actividad52/TaskList.java
@@ -2,10 +2,12 @@ package org.ieslosremedios.daw1.prog.ut5.actividad52;
 
 import java.util.*;
 
-import static java.awt.SystemColor.window;
-
 public class TaskList {
     private List<String> listaTareas;
+
+    public TaskList(List<String> listaTareas) {
+        listaTareas=new ArrayList<>();
+    }
 
     public TaskList(String tipoLista) {
         if (tipoLista.equalsIgnoreCase("ArrayList")){
@@ -15,23 +17,24 @@ public class TaskList {
         }
     }
 
-    public static void addTask(List<String> list, String task){
-        list.add(task);
+    public void addTask(String task){
+        listaTareas.add(task);
         System.out.println("Tarea añadida: "+task);
     }
 
-    public static void removeTask(List<String> list,String task){
-        list.remove(task);
+    public  void removeTask(String task){
+        listaTareas.remove(task);
         System.out.println("Tarea eliminada: "+task);
     }
-    public static void completeTask(List<String> list,String task){
+    public  void completeTask(String task){
         String completado="Tarea completada: ["+ task +"]";
-        list.set(list.indexOf(task), completado);
+        listaTareas.set(listaTareas.indexOf(task), completado);
         System.out.println(completado);
     }
 
-    public static String[] getTasks(List<String> list){
-        String[] array=(String [])list.toArray();
+    public  String[] getTasks(){
+        String [] array=new String[listaTareas.size()];
+        array=listaTareas.toArray(array);
         return array;
     }
 
